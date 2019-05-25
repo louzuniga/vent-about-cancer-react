4e28ffa822261849a47e4fc69bc4c87ca8b2f6c7
Lou Zuniga <louzuniga86@gmail.com>
2019-05-24 19:15:22 -0700

    started redux

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import Landing from './components/Landing';
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Combines react and redux toget by using Provider
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Nav />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Nav />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
